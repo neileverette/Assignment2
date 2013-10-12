aadaa0cab55ef8ce342441ac2bddd88c577d873c
S <s>
2013-10-12 13:31:07 -0700

    {"type":"TI", "runs":"0"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -12,12 +12,34 @@ import java.awt.*;
 
 public class CS106ATiles extends ConsoleProgram  { //Replace with GraphicsProgram 
 	
-	/** Amount of space (in pixels) between tiles */
-	private static final int TILE_SPACE = 20;
+//	/** Amount of space (in pixels) between tiles */
+//	private static final int TILE_SPACE = 20;
 
 	public void run() {
-		/* You fill this in. */
-		println("Peace");
+
+		// This sets how high to 
+		
+		// Variables need for the current number
+		int number1=0;
+		int number2=1;
+		int number3=0;
+		
+		// Caluculate the 
+		while (number3<500){
+			// Add numbers 1 and 2, set them to be the value of number 3
+			number3 = number1 + number2;
+			
+			// Print the results on screen
+			println(number3);
+			
+			// Reset the previous numbers with the new values
+			// Set number 1 to be the value of number 2
+			number1=number2;
+			
+			// Set number 2 to be the value of number 3
+			number2=number3;
+		}
+		
 	}
 }
 
