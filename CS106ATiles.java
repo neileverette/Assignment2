228b900cb2fd470e29c43cadc2a97793b0a5307a
S <s>
2013-10-11 17:37:24 -0700

    {"type":"TI", "runs":"2"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -10,27 +10,14 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
+public class CS106ATiles extends ConsoleProgram  { //Replace with GraphicsProgram 
 	
 	/** Amount of space (in pixels) between tiles */
 	private static final int TILE_SPACE = 20;
 
 	public void run() {
 		/* You fill this in. */
-		
-		GLabel label = new GLabel("Hello World", 100,75);
-		label.setFont("Roboto-Bold-36");
-		label.setColor(Color.RED);
-		
-		GRect rectangle = new GRect(10, 10, 100, 50);
-		
-		GOval oval = new GOval(100, 100, 100, 50);
-		oval.setFilled(true);
-		oval.setFillColor(Color.RED);
-		
-		add(rectangle);
-		add(label);
-		add(oval);
+		println("Peace");
 	}
 }
 
