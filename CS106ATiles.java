02e36d2787a1d4e5e56cb6e907f6b9d75ef4f9fa
S <s>
2013-10-12 16:23:45 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -10,41 +10,18 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class CS106ATiles extends ConsoleProgram  { //Replace with GraphicsProgram 
+public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
 	
 //	/** Amount of space (in pixels) between tiles */
 //	private static final int TILE_SPACE = 20;
 
 	public void run() {
+		
+		GRect myRect = new GRect(10,10,50,50);
+		
+		add(myRect);
 
-		// This sets how high to 
-		int maximumNumber = 10000;
-		
-		// Variables need for the current number
-		int number1=0;
-		int number2=1;
-		int number3=0;
-		
-		
-		// Print the intial values
-		println(number1);
-		println(number2);
-		
-		// Caluculate the fib sequence
-		while (number3<maximumNumber){
-			// Add numbers 1 and 2, set them to be the value of number 3
-			number3 = number1 + number2;
-			
-			// Print the results on screen
-			println(number3);
-			
-			// Reset the previous numbers with the new values
-			// Set number 1 to be the value of number 2
-			number1=number2;
-			
-			// Set number 2 to be the value of number 3
-			number2=number3;
-		}
+	
 		
 	}
 }
