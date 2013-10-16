c57baa321eb7d9e3ccdd7d0f6c76aa2e2ed8f4a2
S <s>
2013-10-15 19:28:24 -0700

    {"type":"TI", "runs":"0"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -10,32 +10,25 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
+public class CS106ATiles extends ConsoleProgram  { //Replace with GraphicsProgram 
 
-	// The number of rows in the layout
-	private static final int nRows = 8;
-	
-	// The number of columns in the layout
-	private static final int nCols = 8;
+	private static final int MAX_NUM=10;
 	
 	public void run(){
-		
-		// Get the width of the window and divide it by the number of rows
-		int sqSize = getHeight()/nRows;
-		
-		// Simple for loop to 
-		for (int i = 0; i < nRows; i++){
-			// This is the inner loop to set the rows
-			for (int j = 0; j < nCols; j++){
-			int x = i * sqSize;
-			int y = j * sqSize;
-			
-			GRect mySquare = new GRect(x, y, sqSize, sqSize);
-			mySquare.setFilled(((i+j)%2) ==0);
-			add(mySquare);
-			}
+		for(int i = 0; i < MAX_NUM; i++){
+			println(i + "!" + factorial(i));
 		}
+	}
 	
+	// Write a function to cover the Factorial
+	private int factorial(int n) {
+		
+		int result = 1;
+		
+		for (int j = 1; j < n; j++){
+			result *=j;
+		}
+		return result;
 	}
 
 }
