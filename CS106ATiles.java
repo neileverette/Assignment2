30a8cf393c2ca0421edbf467a6303e7e8e6d6397
S <s>
2013-10-17 10:26:06 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -9,26 +9,18 @@
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
+import acm.util.*;
 
-public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
-
+public class CS106ATiles extends ConsoleProgram  { //Replace with GraphicsProgram 
+	
 	
 	public void run(){
+		println(rgen);
 		
-		for (int i = 0; i < 5; i++){
-			add(filledCircle(i*100,100, 50, Color.black));
-		}
-
-		
-
 	}
 	
-	private GOval filledCircle(int x, int y, int r, Color color){
-		GOval circle = new GOval(x-r, y-r, 2*r, 2*r);
-		circle.setFilled(true);
-		circle.setFillColor(color);
-		return circle;
-	}
-
+	// This is a private instance variable
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
 }
 
