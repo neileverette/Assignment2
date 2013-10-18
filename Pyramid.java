4242841ba28c3865d0ab839ac22470f0cf04c142
S <s>
2013-10-18 14:39:30 -0700

    {"type":"TI", "runs":"0"}

--- Pyramid.java
+++ Pyramid.java
@@ -35,7 +35,7 @@ public class Pyramid extends GraphicsProgram {
 		// This will loop through the rows
 		for (int i = 0; i < BRICKS_IN_BASE; i++){
 			
-			base = BRICKS_IN_BASE - i;
+			base = BRICKS_IN_BASE--;
 			
 			int xPosition = 0+i*BRICK_WIDTH;
 			GRect myBlock = new GRect(xPosition,BRICK_HEIGHT, BRICK_WIDTH, BRICK_HEIGHT);
