54db2fd7e10422fd0d3fb71a3a2a277d8ac21046
S <s>
2013-10-19 16:59:00 -0700

    {"type":"TI", "runs":"0"}

--- Target.java
+++ Target.java
@@ -13,18 +13,21 @@ import java.awt.*;
 public class Target extends GraphicsProgram {	
 	public void run() {
 		
-		int circle1 = 72;
+		double radius1 = 1*72/2;
+		double radius2 = .65*72/2;
+		double radius3 = .3*72/2;
 		
 		
 		int x = getWidth()/2;
 		int y = getHeight()/2;
 		
-		add(filledCircle(x, y, 2, Color.RED));
-	
+		add(filledCircle(x, y, radius1, Color.RED));
+		add(filledCircle(x, y, radius2, Color.WHITE));
+		add(filledCircle(x, y, radius3, Color.RED));
 		
 	}
 	
-	private GOval filledCircle(int x, int y, int r, Color color){
+	private GOval filledCircle(int x, int y, double r, Color color){
 		GOval circle = new GOval(x-r, y-r, 2*r, 2*r);
 		circle.setFilled(true);
 		circle.setColor(color);
