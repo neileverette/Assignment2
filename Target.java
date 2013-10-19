06cf1d50ef8b674e845953f83caf212c5246ec07
S <s>
2013-10-19 16:43:13 -0700

    {"type":"TI", "runs":"1"}

--- Target.java
+++ Target.java
@@ -12,6 +12,15 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
+		
+		add(circle(10,10,20,20, Color.RED));
+		
+	}
+	
+	private GOval circle(int x, int y, int r, Color color){
+		GOval circle = new GOval(x-r, y-r, 2*r, 2*r);
+		circle.setFilled(true);
+		circle.setColor(color);
+		return circle;
 	}
 }
