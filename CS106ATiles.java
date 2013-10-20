fec67bf250c93cbdf0e7e159203e3b205403c898
S <s>
2013-10-20 09:07:11 -0700

    {"type":"TI", "runs":"2"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -20,7 +20,7 @@ public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgr
 	
 	public void run(){
 	
-		add(box(10, 10, WIDTH, HEIGHT, "My Label"));
+		add(box(10, 10, WIDTH, HEIGHT, "Target"));
 		
 	}
 	
