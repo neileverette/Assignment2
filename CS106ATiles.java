a3cfb202b5f04c8cdba5b0c8a4e6ed0e075a63de
S <s>
2013-10-20 09:57:49 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -16,12 +16,12 @@ import acm.program.*;
 public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
 	
 	private static final int WIDTH = 5;
-	private static final int HEIGHT = 20;
+	private static final int HEIGHT = 10;
 	
 	public void run(){
 		
 		int boxWidth = getWidth()/WIDTH;
-		int boxHeight = getHeight()/WIDTH;
+		int boxHeight = getHeight()/HEIGHT;
 	
 		add(box(200, 10, boxWidth, boxHeight, "Program"));
 		add(box(10, 100, boxWidth, boxHeight, "GraphicsProgram"));
