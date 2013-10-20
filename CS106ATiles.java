f3d8ad1d7fee504d8936d4fa6822d6f187eda9fb
S <s>
2013-10-20 09:08:00 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -20,7 +20,7 @@ public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgr
 	
 	public void run(){
 	
-		add(box(10, 10, WIDTH, HEIGHT, "Target"));
+		add(box(10, 10, WIDTH, HEIGHT, "Program"));
 	}
 	
 	private GRect box(int x, int y, int width, int height, String label){
