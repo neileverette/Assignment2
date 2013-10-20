656eb293a35df2eb813b075028736df79dd236b6
S <s>
2013-10-20 10:22:00 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -15,27 +15,24 @@ import acm.program.*;
 
 public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
 	
-	private static final int WIDTH = 5;
-	private static final int HEIGHT = 10;
+	private static final int WIDTH = 200;
+	private static final int HEIGHT = 100;
 	
 	public void run(){
-		
-		int boxWidth = getWidth()/WIDTH;
-		int boxHeight = getHeight()/HEIGHT;
 	
-		add(box(200, 10, boxWidth, boxHeight, "Program"));
-		add(box(10, 100, boxWidth, boxHeight, "GraphicsProgram"));
+		add(box(10, 10, WIDTH, HEIGHT, "Program"));
+		add(box(10, 100, WIDTH, HEIGHT, "GraphicsProgram"));
 	}
 	
 	private GRect box(int x, int y, int width, int height, String label){
 		GRect myRect = new GRect(x,y, width,height);
-		add(label(label, x, y, width, height));
+		add(label(label, x, y));
 		return myRect;
 	}
 	
-	private GLabel label(String str, int labelX, int labelY, int width, int height ){
+	private GLabel label(String str, int labelX, int labelY ){
 		
-		GLabel myLabel = new GLabel(str, labelX, labelY, width, height );
+		GLabel myLabel = new GLabel(str, labelX, labelY);
 		
 		double labelWidth = myLabel.getWidth();
 		double labelHeight = myLabel.getAscent();
