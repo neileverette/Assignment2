6053f4b1482af142bf3d70a4fa71ee18d70849a9
S <s>
2013-10-21 14:30:37 -0700

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -15,9 +15,16 @@ public class FindRange extends ConsoleProgram {
 	public void run() {
 		
 		while(true){
-			int number = readInt("Enter a number");
-
+			int number = readInt("Enter a number: ");
+			
+			if (number == SENTINEL){
+				println("You hit the Sentinel");
+			}else if (number == SENTINEL - 1 || number == SENTINEL + 1 ){
+				println("You are very close");
+			}
+				
 		}
+		
 	//	println("You hit the SENTINEL");
 	//	println("Low number is " + lowNum);
 	//	println("High number is " + highNum);
