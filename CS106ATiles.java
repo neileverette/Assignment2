d8e37d5c0b0673451be49ddb68438d6ebaa87e49
S <s>
2013-10-20 19:44:02 -0700

    {"type":"TI", "runs":"1"}

--- CS106ATiles.java
+++ CS106ATiles.java
@@ -15,7 +15,7 @@ import acm.program.*;
 
 public class CS106ATiles extends GraphicsProgram  { //Replace with GraphicsProgram 
 	
-	private static final int WIDTH = 200;
+	private static final int WIDTH = 300;
 	private static final int HEIGHT = 100;
 	
 	public void run(){
