a15cfd5eec5b7c33f5ef9bb920d335a57346a51d
S <s>
2013-10-21 18:16:37 -0700

    {"type":"TI", "runs":"1"}

--- FindRange.java
+++ FindRange.java
@@ -14,18 +14,22 @@ public class FindRange extends ConsoleProgram {
 	
 	public void run() {
 		
+		// Header discription for the application
 		println("This application checks for low and high numbers");
 		
+		// Variables for the application
 		int number = readInt("? ");
-		
 		int lowNum = number;
 		int highNum = number;
 		
 		while(true){
 			
+			// Check to see if the number entered is the Sentinel
 			if(number == SENTINEL){
 				println("You hit the SENTINEL");
 				break;
+				
+			// This checks to see if the number is close to the Sentinel
 			}else if (number-1 == SENTINEL){
 				println("You are very close");
 				highNum = number;
@@ -34,15 +38,18 @@ public class FindRange extends ConsoleProgram {
 				lowNum = number;
 			}
 			
+			// Once checking for the Sentinel, ask the user for another number
 			number = readInt("? ");
 			
+			// Check and replace high and low numbers
 			if (number < lowNum){
 				lowNum = number;
 			}else if (number > highNum){
 				highNum = number;
 			}
 		}
-			
+		
+		// Once the Sentinel is guessed, the code will break and display the low and high numbers
 		println("Low number is " + lowNum);
 		println("High number is " + highNum);
 	}
