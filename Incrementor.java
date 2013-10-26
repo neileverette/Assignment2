58b5d2642f1064432c5b2ffa06946b75d23ede24
S <s>
2013-10-26 08:37:49 -0700

    {"type":"TI", "runs":"0"}

--- Incrementor.java
+++ Incrementor.java
@@ -1,4 +1,20 @@
 
 public class Incrementor {
+	
+	public Incrementor(int startValue){
+		counter = startValue;
+	}
+	
+	public Incrementor(){
+		counter = 1;
+	}
+	
+	private int nextValue(){
+		int temp = counter;
+		counter++;
+		return temp;
+	}
+
+	private int counter;
 
 }
