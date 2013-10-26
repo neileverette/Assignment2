9af38b95c2eeceb1cb7cdba19b846c54dee4bffd
S <s>
2013-10-26 08:23:35 -0700

    {"type":"TI", "runs":"0"}

--- Incrementer.java
+++ Incrementer.java
@@ -1,4 +1,20 @@
 
 public class Incrementer {
 
-}
+		
+		public Incrementer(int startValue){
+			counter = startValue;
+		}
+		
+		public Incrementer(){
+			counter = 1;
+		}
+		
+		private int nextValue(){
+			int temp = counter;
+			counter++;
+			return temp;
+		}
+
+		private int counter;
+	}
