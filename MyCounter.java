efb5313f175f7f02c780a63fccfa011da21a3606
S <s>
2013-10-26 08:20:23 -0700

    {"type":"TI", "runs":"0"}

--- MyCounter.java
+++ MyCounter.java
@@ -1,4 +1,19 @@
 
 public class MyCounter {
+	
+	public MyCounter(int startValue){
+		counter = startValue;
+	}
+	
+	public MyCounter(){
+		counter = 1;
+	}
+	
+	private int nextValue(){
+		int temp = counter;
+		counter++;
+		return temp;
+	}
 
+	private int counter;
 }
