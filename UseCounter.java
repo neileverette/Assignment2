db8a789889de98380af8cad6291aa9e11ee5259f
S <s>
2013-10-26 10:39:38 -0700

    {"type":"TI", "runs":"1"}

--- UseCounter.java
+++ UseCounter.java
@@ -7,9 +7,16 @@ public class UseCounter extends ConsoleProgram{
 	public void run() {
 		
 		Incrementor count1 = new Incrementor(); // starts at 1
+		Incrementor count2 = new Incrementor(1000);
 		
 		println("Five values for count1:");
 		countFiveTimes(count1);
+		
+		println("Five values for count2:");
+		countFiveTimes(count2);
+		
+		
+		
 		
 	}
 	
