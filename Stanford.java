1b73897f0ae96e693b89d1f779a0b77d918b3891
S <s>
2013-10-26 17:06:30 -0700

    {"type":"TI", "runs":"0"}

--- Stanford.java
+++ Stanford.java
@@ -1,12 +1,31 @@
 
-public class Stanford {
+import acm.program.*;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Stanford extends ConsoleProgram{
+	
+	public void run(){
+		
+		Student stud = new Student("Ben Smith", 1001);
+		stud.setUnits(179);
+		
+		println(stud.getName() + " has "+ stud.getUnits());
+		
+		println(stud.getName() + " can graduate " + stud.hasEnoughUnits());
+		
+		println(stud.getName() + " takes CS106A");
+		
+		stud.incrementUnits(5);
+		
+		println(stud.getName() + " can graduate " + stud.hasEnoughUnits());
+		
+		if (stud.hasEnoughUnits()){
+			println("Good times for " + stud.getName());
+		}
+		
+		
+		
+		
 	}
+	
 
 }
