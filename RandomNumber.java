463673705902d63a57d345a1479bc145b1da1a7e
S <s>
2013-10-28 06:04:19 -0700

    {"type":"TI", "runs":"0"}

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,12 +1,23 @@
 
-public class RandomNumber {
+import acm.program.ConsoleProgram;
+import acm.util.*;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class RandomNumber extends ConsoleProgram {
+	
+	public void run(){
+		
+		// Variables
+		int r = rgen.nextInt(5, 50);
+		int x = rgen.nextInt(0, getWidth()-2*r);
+		
+	while(x < 5){
+			println(x);
+			x = rgen.nextInt(0, getWidth()-2*r);
+		}
+		
 	}
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
 
 }
